c944115dcf98d1ead5f85cfe1556d8c78efc71cc
Maria <marydrinkscoffee@gmail.com>
2025-07-27 18:03:07 +0200

    Update homework-solutions/js/hw-3/task-1.js
    Co-authored-by: Volodymyr Lazurenko <103571439+VladimirLQA@users.noreply.github.com>

--- homework-solutions/js/hw-3/task-1.js
+++ homework-solutions/js/hw-3/task-1.js
@@ -62,7 +62,7 @@ item_6 = 15;
 item_6_type = typeof item_6;
 
 //21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
-console.log('—— "item_6 == ' + item_6 + '", "item_6_type ==" ' + 'item_6_type ——');
+console.log('—— "item_6 == ' + typeof item_6 + '", "item_6_type =="  + typeof item_6_type);
 
 //22. Создать переменную item_7 и в ней преобразовать item_6 в String.
 let item_7;
