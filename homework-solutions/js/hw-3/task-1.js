471faa98df149a6c5c3240c16c1d0f32df6cef50
Maryia Traitsiak <m.traitsiak@andersenlab.com>
2025-07-27 18:10:25 +0200

    updated based on comment

--- homework-solutions/js/hw-3/task-1.js
+++ homework-solutions/js/hw-3/task-1.js
@@ -62,7 +62,7 @@ item_6 = 15;
 item_6_type = typeof item_6;
 
 //21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
-console.log('—— "item_6 == ' + typeof item_6 + '", "item_6_type =="  + typeof item_6_type);
+console.log('—— "item_6 == ' + typeof item_6 + '", "item_6_type ==" ' + 'item_6_type ——');
 
 //22. Создать переменную item_7 и в ней преобразовать item_6 в String.
 let item_7;
@@ -77,4 +77,4 @@ let item_7_type;
 item_7_type = typeof item_7;
 
 //25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——  
-console.log('—— "item_7 == ' + item_7 + '", "item_7_type ==" ' + ' item_7_type ——');
+console.log('—— "item_7 == ' + typeof item_7 + '", "item_7_type ==" ' + ' item_7_type ——');
