88467785e33aec2459fcee047ad4807d9e6a3be2
Marydrinkscoffee <marydrinkscoffee@gmail.com>
2025-08-11 16:47:24 +0200

    HW-6: task-3

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -7,5 +7,12 @@
   Присвойте результат в переменную "unique"
 */
 let unique;
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+unique = [...new Set(numbers)];
+console.log(unique);
+
+
+
+
 
 export { unique };
